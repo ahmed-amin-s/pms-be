1615d7fe7a2eb9e1921ccb477736846ed36b7b7f
Ahmed Amin <100802344+ahmed-amin-s@users.noreply.github.com>
2025-01-02 18:05:38 +0200

    Add files via upload

---
+++ DatabaseCreationScript.sql
@@ -0,0 +1,25 @@
+USE [PmsDb]
+GO
+
+/****** Object:  Table [dbo].[Products]    Script Date: 1/2/2025 6:00:49 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Products](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Description] [nvarchar](500) NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+	[ImageURL] [nvarchar](max) NULL,
+	[CreatedDate] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
